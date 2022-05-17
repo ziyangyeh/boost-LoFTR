257208c5cc3e47b5bfe7f2430279e212883fb463
Ziyang Ye <yeziyang159@gmail.com>
2022-05-17 16:33:09 +0800

    boost-loftr

--- src/loftr/loftr_module/quadtree_attention.py
+++ src/loftr/loftr_module/quadtree_attention.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from timm.models.layers import DropPath, to_2tuple, trunc_normal_
 
-sys.path.append("../QuadTreeAttention")
+sys.path.append("./QuadTreeAttention")
 from QuadtreeAttention.modules.quadtree_attention import QTAttA, QTAttB
 
 
